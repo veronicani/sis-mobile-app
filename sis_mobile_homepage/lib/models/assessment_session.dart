0bb01799ddd98eb41e5fc64bbae6c18ade376073
Veronica Ni <vni1294@gmail.com>
2024-02-22 21:06:51 -0500

    Added docstrings

--- sis_mobile_homepage/lib/models/assessment_session.dart
+++ sis_mobile_homepage/lib/models/assessment_session.dart
@@ -1,7 +1,6 @@
 import 'package:date_time_format/date_time_format.dart';
 
 /// AssessmentSession: {id, title, status, apiUrl}
-
 class AssessmentSession {
   final int id;
   final String title;
@@ -22,8 +21,10 @@ class AssessmentSession {
       required this.endAt
     });
 
+  /// Creates an instant of assessment session from JSON data.
   factory AssessmentSession.fromJson(Map<String, dynamic> json){
-
+    /// convertUtcToLocal: Converts a string in UTC time to USA local time.
+    ///   E.g. 2024-02-06T17:00:04.287983-08:00 -> Tues, Feb 6, 8:00
     String convertUtcToLocal(String utcString){
       DateTime convertToDateTime = DateTime.parse(utcString);
       return convertToDateTime.format('D, M j, H:i');
